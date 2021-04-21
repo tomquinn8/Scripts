2e186bc2645e4347bef81206a80139c098056341
Tom Quinn <tom.quinn@teeque.co.uk>
2021-04-21 11:29:56 +0100

    Create HTTP-Response.py to get http response codes and Location headers for 30x redirects.

---
+++ HTTP-Response.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Take URLs in on std in and (when a response is received) output the url and the "final destination" after redirects have been followed
+
+import sys
+import requests
+import threading
+import concurrent.futures
+
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+def getRequest(url):
+    try:
+        response = requests.get(url, timeout=10, verify = False, allow_redirects=False)
+        if response:
+            redirect = '|' + str(response.headers['Location']) if '30' in str(response.status_code) else ''
+            print (url + '|' + str(response.status_code) + redirect)
+    except Exception:
+        pass
+
+urls = []
+for line in sys.stdin:
+    urls.append(line.rstrip())
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(getRequest, urls)
