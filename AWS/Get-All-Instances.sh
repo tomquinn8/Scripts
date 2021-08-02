45ee544162dfa87b046565b6ef7b69dd37f2c67e
Tom Quinn <tom.quinn@teeque.co.uk>
2021-08-02 11:14:58 +0100

    Modified Get-All-Instances script

--- AWS/Get-All-Instances.sh
+++ AWS/Get-All-Instances.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+# List all EC2 instances
+
 aws ec2 describe-instances --query "Reservations[*].Instances[*].{InstanceId:InstanceId,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" --output table
