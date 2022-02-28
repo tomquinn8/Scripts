5f861b037cfe8eb27c894b4ce60946a329fbf64c
Tom Quinn <tom.quinn@teeque.co.uk>
2022-02-28 12:11:43 +0000

    Add polybar to toggle monitor script

--- Toggle-External-Monitor.sh
+++ Toggle-External-Monitor.sh
@@ -3,6 +3,7 @@
 if xrandr --query | grep 'HDMI-A-0 connected';
 then
     xrandr --output HDMI-A-0 --auto --left-of eDP --primary
+    ~/.config/polybar/launch.sh
 else
     xrandr --output HDMI-A-0 --off
 fi
